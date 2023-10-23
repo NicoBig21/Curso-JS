437703cf022a2dca145e971ac3ebb771b454811d
Nicolas Bignone <nicobignone21@gmail.com>
2023-10-23 20:49:17 -0300

    first commit

---
+++ assets/js/arreglos.js
@@ -0,0 +1,14 @@
+// ARRAYS
+
+let arrregloCosas = [
+    true,
+    123,
+    'Fernando',
+    1 + 2,
+    function () {},
+    ['X', `Megaman`,
+`Zero`, `Dr lemom`]
+];
+
+console.log(arrregloCosas[5][3]);
+
---
+++ assets/js/primitivos.js
@@ -0,0 +1,15 @@
+ //Primitivos
+/* Los primitivos son inmutables y no son un objeto 
+    Boolean: Verdadero y falso 
+    Null: Si un valor es nulo 
+    Undefined: Una variable declarada que aun no se le asigna un valor 
+    Number: Los datos numericos
+    String: Cadena de caracteres ""
+    Symbol: 
+ */
+
+    //Primitivos EJERCICIOSL:
+/*  let nombre = `Peter Parker`; 
+*/
+
+// los prompt, confirm y alerts no se pueden usar para la terminal 
