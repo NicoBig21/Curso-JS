90f3fa323a7fc907fe07130a2f33daa3e8a3165c
NicolasBignone <nicolas.bignone@davinci.edu.ar>
2023-10-30 22:59:59 -0300

    Actualizacion

---
+++ javascript-basics-0.0.2/assets/js/bases/ejercicios.js
@@ -0,0 +1,52 @@
+// ------- EJERCICIO MODULO 3 CLASE RETORNO DE LAS FUNCIONES
+
+/* function getAleatorio() {
+    return Math.random();
+} */
+
+// Convertimos en una funcion flecha 
+
+/* let getAleatorio = () => Math.random();
+
+let valorAleatorio = getAleatorio();
+console.log(valorAleatorio); // Esto imprimirá un número aleatorio entre 0 (inclusive) y 1 (exclusivo). */
+
+// ------- EJERCICIO MODULO 4 CLASE ALTERNATIVA DE IF
+
+/* function dia(numero) {
+    const dias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    return dias[numero];
+}
+
+const hoy = new Date();
+const numeroDia = hoy.getDay();
+const nombreDia = dia(numeroDia);
+
+console.log(nombreDia);  */
+
+//---------- MAS RESUMIDO
+
+/* const dias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+const hoy = new Date();
+const numeroDia = hoy.getDay();
+const nombreDia = dias[numeroDia];
+
+console.log(nombreDia); // Esto imprimirá el nombre del día actual */
+
+// ------- EJERCICIO MODULO 4 OPERADOR TERNARIO 
+
+/* 
+DIAS DE SEMANA ABRIMOS A LAS 11
+FINES DE SEMANA ABRIMOS A LAS 9 
+ */
+
+// Entra a un sitio web, para consultar si esta abierto hoy 
+
+
+
+
+
+
+
+
+
